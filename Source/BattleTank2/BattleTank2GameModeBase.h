37fbb586751fe3dc3131bf43a8572e1040cd387f
Ricardo Dias <ricardodc@gmail.com>
2016-11-22 02:20:50 -0200

    Initial Commit

---
+++ Source/BattleTank2/BattleTank2GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK2_API ABattleTank2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
